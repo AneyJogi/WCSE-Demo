56574e0d3c7c4659ff382a976a98036d07797fef
alwin <alwin@Aney>
2023-01-15 05:12:45 +0530

    comit 15/01

--- src/test/java/DemoMain/GenericSample.java
+++ src/test/java/DemoMain/GenericSample.java
@@ -6,19 +6,10 @@ public class GenericSample {
 	{
 		System.out.println("This is generic");
 
-
-
-
 		System.out.println("This is aney");
 		
 		System.out.println("doing push on 15/01");
 		
-		
-
-		
-		
-		
-
 		
 		
 		
